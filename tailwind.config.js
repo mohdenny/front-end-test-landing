0a4b410d8543d9b8d204c604cb4c9fb701313db2
Mohammad Denny <mohammaddenny94@gmail.com>
2021-09-24 15:51:25 +0700

    config tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,22 @@
 module.exports = {
-  purge: [],
+  purge: [
+    './src/**/*.html',
+    './src/**/*.js',
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
-    fontFamily: {
-      'headline': ['Tahu!'],
-      'sub-headline': ['Gotham-Medium'],
-    }
+    extend: {
+      fontFamily: {
+        'tahu': ['Tahu!', 'Tahu! Regular'],
+        // 'gotham-medium': ['Gotham-Medium'],
+      },
+      height: {
+        'hero-lg' : '490px',
+      },
+      // backgroundImage: {
+      //   'hero-vector-pattern': "url('./src/assets/images/heros/Vector.png')"
+      // }
+    },
   },
   variants: {
     extend: {},
