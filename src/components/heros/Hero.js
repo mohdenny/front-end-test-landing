d8c7f4d8cbb4e1e0ce16d527a5de9cebfad7a3fe
Mohammad Denny <mohammaddenny94@gmail.com>
2021-09-24 15:50:54 +0700

    add hero element

--- src/components/heros/Hero.js
+++ src/components/heros/Hero.js
@@ -3,11 +3,11 @@ import React, { Fragment } from 'react';
 const Hero = () => {
     return (
         <Fragment>
-            <div id="hero" className="h-600 max-w-full">
+            <div id="hero" className="h-hero-lg max-w-full">
                 <div id="shape_1" className="bg-red-600 h-full text-center">
-                    <div id="vector" className="h-full text-center">
+                    <div id="vector" className="bg-hero-vector-pattern h-full text-center">
                         <div id="headline" className="h-auto w-full px-8 py-4">
-                            <div id="logo" className="grid grid-cols-6  w-full h-full text-center mb-2">
+                            <div id="logo" className="grid grid-cols-6 w-full h-full text-center mb-2">
                                 <div className="bg-yellow-500 col-start-1 h-14 text-center">
                                     Logo Bisnis
                                 </div>
@@ -16,10 +16,10 @@ const Hero = () => {
                                 </div>
                             </div>
                             <div id="text" className="grid grid-rows-2 gap-2 w-full h-full text-center text-white">
-                                <div className="h-full px-4 py-2 text-7xl text-center font-headline">
+                                <div className="h-full px-4 py-2 text-7xl text-center font-tahu">
                                     Spirit Membangun Indonesia Maju !
                                 </div>
-                                <div className="h-full px-4 py-2 text-center text-white font-sub-headline">
+                                <div className="h-full px-4 py-2 text-center text-white font-gotham-medium">
                                     Di tengah pro kontra mengenai layak tidaknya Indonesia menyandang predikat negara maju,<br/> 
                                     menjadi menarik untuk mengetahui seberapa jauh kemajuan yang dicapai setelah negara ini<br/>
                                     merdeka selama 75 tahun. Apa peluang dan tantangan Indonesia menjadi negara maju di saat<br/>
