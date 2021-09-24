a843308df5c6d81b4249e698a31a5ad9108ef247
Mohammad Denny <mohammaddenny94@gmail.com>
2021-09-24 15:51:06 +0700

    import tailwindcss

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './assets/styles/tailwind.css';
-import './assets/styles/style.css';
+import '../src/assets/styles/tailwind.css';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
