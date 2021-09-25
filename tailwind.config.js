36b2429b8b28a8dbd7195583510a9932a1e9b615
Mohammad Denny <mohammaddenny94@gmail.com>
2021-09-26 01:52:44 +0700

    configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,15 +7,34 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        'tahu': ['Tahu!', 'Tahu! Regular'],
-        // 'gotham-medium': ['Gotham-Medium'],
+        'tahu': ['Tahu'],
+        'gotham': ['Gotham'],
+      },
+      fontSize: {
+        '2xs': '0.5rem',
+        '3xs': '0.4rem',
+        '4xs': '0.3rem'
       },
       height: {
-        'hero-lg' : '490px',
+        'hero-sm' : '340px',
+        'hero-md' : '410px',
+        'hero-lg' : '550px',
+        'hero-xl' : '670px',
+        'hero-2xl' : '820px',
+        'video-thumb-sm' : '180px',
+        'video-thumb-md' : '280px',
+        'video-thumb-lg' : '380px',
+        'video-thumb-xl' : '480px',
+        'video-thumb-2xl' : '580px',
       },
-      // backgroundImage: {
-      //   'hero-vector-pattern': "url('./src/assets/images/heros/Vector.png')"
-      // }
+      backgroundImage: {
+        'shape-red': "url('../images/heros/shape_red.png')",
+        'vector-pattern': "url('../images/heros/vector.png')",
+        'logo-bisnis-top': "url('../images/heros/logo_bisnis_top.png')",
+        'logo-indonesia-maju-top': "url('../images/heros/logo_indonesia_maju_top.png')",
+        'video-thumb': "url('../images/heros/video_thumb.png')",
+        'icon-player': "url('../images/heros/icon_player.png')",
+      }
     },
   },
   variants: {
