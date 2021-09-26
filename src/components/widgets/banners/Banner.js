01f581095ad3de8caa77a127d8a1e52183cfe0bc
Mohammad Denny <mohammaddenny94@gmail.com>
2021-09-26 22:26:27 +0700

    banner add text

--- src/components/widgets/banners/Banner.js
+++ src/components/widgets/banners/Banner.js
@@ -5,8 +5,10 @@ const Banner = () => {
         <div id="banner" className="grid grid-cols-12 border-8 border-solid border-gray-800">
             <div id="text" className="bg-shape bg-no-repeat bg-contain h-presiden-banner col-start-3 col-end-11 border-8 border-solid border-gray-800">
                 <div id="text" className="bg-presiden-banner bg-no-repeat bg-contain h-presiden-banner col-start-3 col-end-11 text-white text-center border-8 border-solid border-gray-800">
-                    <p>75 Tahun Indonesia Merdeka</p>
-                    <p>"BAGIMU NEGERI"</p>
+                    <div className="my-auto">
+                        <p>75 Tahun Indonesia Merdeka</p>
+                        <p>"BAGIMU NEGERI"</p>
+                    </div>
                 </div>
             </div>
         </div>
